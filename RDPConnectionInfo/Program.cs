fa0ea259f7ecce600b767da30244818bb37f49ed
Janl1 <service@janl1.de>
2017-10-01 15:56:08 +0200

    Init

---
+++ RDPConnectionInfo/Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using IniParser;
+using IniParser.Model;
+using System.Security;
+
+namespace RDPConnectionInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FileIniDataParser parser;
+                IniData data;
+                if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "config.ini"))
+                {
+                    StreamWriter ws = File.AppendText(AppDomain.CurrentDomain.BaseDirectory + "config.ini");
+                    ws.WriteLine("[Settings]");
+                    ws.WriteLine("MessagesCaption = \"WARNING!\"");
+                    ws.WriteLine("MessageText = \"There is currently a user connected to this rdp session! If you countinue, he will get kicked out!\"");
+                    ws.Flush();
+                    ws.Close();
+                }
+
+                if (args.Length != 1)
+                {
+                    Console.WriteLine("RDPConnectionInfo V.0.0.1\nUsage: <RDPConnectionInfo>.exe start|stop");
+                    Console.ReadLine();
+                    return;
+                }
+
+                parser = new FileIniDataParser();
+                data = parser.ReadFile(AppDomain.CurrentDomain.BaseDirectory + "config.ini");
+
+                if (args[0] == "start")
+                {
+                    try
+                    {
+                        RegistryKey myKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", true);
+                        if (myKey != null)
+                        {
+                            myKey.SetValue("legalnoticetext", data["Settings"]["MessageText"].Replace("\"", ""), RegistryValueKind.String);
+                            myKey.SetValue("legalnoticecaption", data["Settings"]["MessagesCaption"].Replace("\"", ""), RegistryValueKind.String);
+                            myKey.Close();
+                        }
+                        Console.WriteLine("Done");
+                    }
+                    catch (SecurityException ex)
+                    {
+                        Console.WriteLine("Fehler: {0}", ex.GetBaseException());
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+                else if (args[0] == "stop")
+                {
+                    try
+                    {
+                        RegistryKey myKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", true);
+                        if (myKey != null)
+                        {
+                            myKey.SetValue("legalnoticetext", "", RegistryValueKind.String);
+                            myKey.SetValue("legalnoticecaption", "", RegistryValueKind.String);
+                            myKey.Close();
+                        }
+                        Console.WriteLine("Done");
+                    }
+                    catch(SecurityException ex)
+                    {
+                        Console.WriteLine("Fehler: {0}", ex.GetBaseException());
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+            } catch(Exception ex)
+            {
+                Console.WriteLine("Fehler: {0}", ex.GetBaseException());
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RDPConnectionInfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("RDP-Kick-Prevention")]
+[assembly: AssemblyDescription("RDP Kick Prevention ist eine Lösung, um mit mehreren Personen einen Windows RDP-Account zu nutzen")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Janl1")]
+[assembly: AssemblyProduct("RDP-Kick-Prevention")]
+[assembly: AssemblyCopyright("Janl1.DE ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(true)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("becee1c5-bea2-4673-8c73-2094c20447e0")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
